7848333ef9b9788899b76fb06b80b6a3322087af
shubh <harry@localhost>
2020-07-17 05:23:45 +0000

    stash file

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+mile= input("how far you want to travel? ")
+mile=int(mile)
+if mile<=3:
+	print("please walk")
+elif 300>=mile>=4:
+	print ("please drive car")
+else:
+	print("flying to your destination")
+print ("have  agood day")
+
